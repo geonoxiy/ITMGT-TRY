b8f4a87c457ac28a8877d95ee31c8b0c489afdff
geonoxiy <love.almarie.caranza@student.ateneo.edu>
2024-07-19 08:25:02 +0800

    Update App.py

--- App.py
+++ App.py
@@ -163,3 +163,4 @@ local_tz = pytz.timezone('Asia/Manila')
 local_time = datetime.datetime.now(local_tz)
 
 st.write("Last updated:", local_time.strftime("%Y-%m-%d %H:%M:%S"))
+st.write("Occasionally refresh the website to get updates!")
