e6992d664ef05bcbb91b14dd78200e43263db98d
geonoxiy <love.almarie.caranza@student.ateneo.edu>
2024-07-19 08:13:17 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import streamlit as st
-import time
 import datetime
 import pytz
-import matplotlib
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 st.set_page_config(
@@ -12,32 +10,33 @@ st.set_page_config(
     page_icon="https://cdn-icons-png.freepik.com/512/6984/6984901.png"
 )
 
-# Cache the data loading function to avoid reloading data on each interaction
-@st.cache_data(ttl=5)
+@st.cache_data(ttl=300)
 def load_data(sheet_id):
-    return pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+    return pd.read_csv(url)
 
 sheet_idA = '16CwByzI3-J0o36W7vs4hZ1Ovmyc2uV0DhJH4Cj96rU8'
 dfA = load_data(sheet_idA)
 
-line = st.selectbox(label=("Select an E-Jeep Line"), options=["LINE A", "LINE B"])
+line = st.selectbox(label="Select an E-Jeep Line", options=["LINE A", "LINE B"])
 
 def plot_map(title, coords, place_coords, place_labels):
-    fig, ax = plt.subplots(figsize=(6, 4))  # Adjust the width and height to make the figures smaller
+    fig, ax = plt.subplots(figsize=(6, 4))
     icon_path = 'pin.png'
     icon = plt.imread(icon_path)
-    # Add icons and labels to the map
+
     for (x, y), label in zip(place_coords, place_labels):
         im = OffsetImage(icon, zoom=0.005)
         ab = AnnotationBbox(im, (x, y), xycoords='data', frameon=False)
         ax.add_artist(ab)
         ax.text(x + 0.1, y + 0.25, f' {label}', fontsize=6, verticalalignment='center_baseline', zorder=10)
+    
     ax.plot(*zip(*coords), color='lightgray', label='Route')  
     ax.set_title(title, fontsize=10, pad=20)
     ax.axis('off')
     return fig, ax
 
-def highlight_route(ax, start, end, line_coords):
+def highlight_route(ax, start, end, line_coords, color):
     start_index = line_coords["coords"].index(line_coords["place_coords"][line_coords["place_labels"].index(start)])
     end_index = line_coords["coords"].index(line_coords["place_coords"][line_coords["place_labels"].index(end)])
     
@@ -50,7 +49,7 @@ def highlight_route(ax, start, end, line_coords):
         highlighted_y = [line_coords["coords"][i][1] for i in range(start_index, len(line_coords["coords"]))]
         highlighted_y += [line_coords["coords"][i][1] for i in range(0, end_index + 1)]
     
-    ax.plot(highlighted_x, highlighted_y, color='#0305C6', linewidth=1, label=f'Route {start} to {end}')
+    ax.plot(highlighted_x, highlighted_y, color=color, linewidth=1, label=f'Route {start} to {end}')
     ax.legend(fontsize=6)
 
 line_coords = {
@@ -59,7 +58,7 @@ line_coords = {
         "place_coords": [(15, 1), (10, 3), (8, 4), (11, 4.5), (12.5, 5), (14.5, 5), (15, 1)],
         "place_labels": ['Hagdan na Bato', 'Old Comm', 'Gate 1', 'Gate 2.5', 'Leong Hall', 'Xavier Hall', 'Hagdan na Bato']
     }, 
-    "LINE B":{
+    "LINE B": {
         "coords": [(11,1),(15,1),(15,10),(15,17),(15,20),(14,20),(14,7.5),(10,7.5),(8,7.5),(8,5),(9,5),(9,1),(11,1)],
         "place_coords": [(11,1),(15,10),(15,17),(10,7.5),(8,5), (11, 1)],
         "place_labels": ['Xavier Hall','AJHS','ASHS FLC Building','ISO','Arete', 'Xavier Hall']
@@ -71,8 +70,8 @@ if line == "LINE A":
     st.write("If an E-jeep is marked For Charging, its final stop will be at Gate 1. The E-jeep will continue to make all stops up to Gate 1, as indicated on the map below.")
     st.write(dfA.head(3))
     
-    # Plot A1, A2, A3 on one map
     fig, ax = plot_map("Line A Routes", line_coords["LINE A"]["coords"], line_coords["LINE A"]["place_coords"], line_coords["LINE A"]["place_labels"])
+    
     try:
         A1 = dfA.iloc[0, 1]
         A2 = dfA.iloc[1, 1]
@@ -80,26 +79,26 @@ if line == "LINE A":
         N1 = dfA.iloc[0, 2]
         N2 = dfA.iloc[1, 2]
         N3 = dfA.iloc[2, 2]
-        
-        def highlight(ax, last_item, next_item):
-            if last_item == next_item:
-                highlight_route(ax, last_item, last_item, line_coords["LINE A"])
-            elif last_item == "Hagdan na Bato":
-                highlight_route(ax, "Hagdan na Bato", "Old Comm", line_coords["LINE A"])
-            elif last_item == "Old Comm":
-                highlight_route(ax, "Old Comm", "Gate 1", line_coords["LINE A"])
-            elif last_item == "Gate 1":
-                highlight_route(ax, "Gate 1", "Gate 2.5", line_coords["LINE A"])
-            elif last_item == "Gate 2.5":
-                highlight_route(ax, "Gate 2.5", "Leong Hall", line_coords["LINE A"])
-            elif last_item == "Leong Hall":
-                highlight_route(ax, "Leong Hall", "Xavier Hall", line_coords["LINE A"])
-            elif last_item == "Xavier Hall":
-                highlight_route(ax, "Xavier Hall", "Hagdan na Bato", line_coords["LINE A"])
 
-        highlight(ax, A1, N1)
-        highlight(ax, A2, N2)
-        highlight(ax, A3, N3)
+        def highlight(ax, last_item, next_item, color):
+            if last_item == next_item:
+                highlight_route(ax, last_item, last_item, line_coords["LINE A"], color)
+            elif last_item == "Hagdan na Bato":
+                highlight_route(ax, "Hagdan na Bato", "Old Comm", line_coords["LINE A"], color)
+            elif last_item == "Old Comm":
+                highlight_route(ax, "Old Comm", "Gate 1", line_coords["LINE A"], color)
+            elif last_item == "Gate 1":
+                highlight_route(ax, "Gate 1", "Gate 2.5", line_coords["LINE A"], color)
+            elif last_item == "Gate 2.5":
+                highlight_route(ax, "Gate 2.5", "Leong Hall", line_coords["LINE A"], color)
+            elif last_item == "Leong Hall":
+                highlight_route(ax, "Leong Hall", "Xavier Hall", line_coords["LINE A"], color)
+            elif last_item == "Xavier Hall":
+                highlight_route(ax, "Xavier Hall", "Hagdan na Bato", line_coords["LINE A"], color)
+
+        highlight(ax, A1, N1, "red")
+        highlight(ax, A2, N2, "blue")
+        highlight(ax, A3, N3, "green")
 
         ax.legend(fontsize=6, bbox_to_anchor=(1.05, 1), loc='upper left')
         st.pyplot(fig)
@@ -113,52 +112,6 @@ if line == "LINE A":
         st.write('A2: This E-jeep is only until Gate 1. This will still pass through stops before Gate 1.')
     if dfA.iloc[2, 3] == "For Charging":
         st.write('A3: This E-jeep is only until Gate 1. This will still pass through stops before Gate 1.')
-
-if line == "LINE B":
-    st.title("Line B")
-    st.write("If an E-jeep is marked For Charging, its final stop will be at Xavier Hall. The E-jeep will continue to make all stops up to Xavier Hall, as indicated on the map below.")
-    st.write(dfA.iloc[5:8])
-    
-    # Plot B1, B2, B3 on one map
-    fig, ax = plot_map("Line B Routes", line_coords["LINE B"]["coords"], line_coords["LINE B"]["place_coords"], line_coords["LINE B"]["place_labels"])
-    try:
-        B1 = dfA.iloc[5, 1]
-        B2 = dfA.iloc[6, 1]
-        B3 = dfA.iloc[7, 1]
-        M1 = dfA.iloc[5, 2]
-        M2 = dfA.iloc[6, 2]
-        M3 = dfA.iloc[7, 2]
-        
-        def highlight(ax, last_item, next_item):
-            if last_item == next_item:
-                highlight_route(ax, last_item, last_item, line_coords["LINE B"])
-            elif last_item == "Xavier Hall":
-                highlight_route(ax, "Xavier Hall", "AJHS", line_coords["LINE B"])
-            elif last_item == "AJHS":
-                highlight_route(ax, "AJHS", "ASHS FLC Building", line_coords["LINE B"])
-            elif last_item == "ASHS FLC Building":
-                highlight_route(ax, "ASHS FLC Building", "ISO", line_coords["LINE B"])
-            elif last_item == "ISO":
-                highlight_route(ax, "ISO", "Arete", line_coords["LINE B"])
-            elif last_item == "Arete":
-                highlight_route(ax, "Arete", "Xavier Hall", line_coords["LINE B"])
-
-        highlight(ax, B1, M1)
-        highlight(ax, B2, M2)
-        highlight(ax, B3, M3)
-        
-        ax.legend(fontsize=6, bbox_to_anchor=(0, 1), loc='upper left')
-        st.pyplot(fig)
-        
-    except Exception as e:
-        st.error(f"Error highlighting route: {e}")
-    
-    if dfA.iloc[5, 3] == "For Charging":
-        st.write('B1: This E-jeep is only until Xavier Hall.')
-    if dfA.iloc[6, 3] == "For Charging":
-        st.write('B2: This E-jeep is only until Xavier Hall.')
-    if dfA.iloc[7, 3] == "For Charging":
-        st.write('B3: This E-jeep is only until Xavier Hall.')
 
 local_tz = pytz.timezone('Asia/Manila')
 local_time = datetime.datetime.now(local_tz)
